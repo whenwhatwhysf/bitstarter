84ece429c2282cca1f02efef054b0e12abf24d94
Kevin Prodehl <whenwhatwhysf@gmail.com>
2013-07-11 21:49:53 +0000

    Index.html read in web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
-
+var buffer = new fs.readFileSync('index.html');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
